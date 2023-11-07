37ca04bf77bd869c5a07b07b61f1a49fea21cc35
Ktosox <ktoso3@gmail.com>
2023-11-07 21:47:21 +0100

    tried expanding on the controller
    screen rotation with mouse is more of a faf that I expected

--- Project/AdminStuff/welcome_screen.gd
+++ Project/AdminStuff/welcome_screen.gd
@@ -1,5 +1,9 @@
 extends Control
 
+# list all the scenes from the StyleScenes folder
+# display them in an easy to select way
+
+@export var ScenesGoHere : Array[NodePath]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
