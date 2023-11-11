5114fc4d44a2f823553abf79b15d9d6b445bb8f5
Ktosox <ktoso3@gmail.com>
2023-11-11 12:41:53 +0100

    cut out dragging since I need to re-evaulate it

--- Project/AdminStuff/camera_controller.gd
+++ Project/AdminStuff/camera_controller.gd
@@ -4,22 +4,15 @@ extends Camera3D
 
 @export var speed = 24.0
 
-var dragMousePos
-var dragStartPos
-var dragging = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$ScreenRef.get_local_mouse_position()
+
 	pass # Replace with function body.
 
 func _input(event):
 	if event.is_action_pressed("reset"):
 		global_transform = initalCameraState
-	if event.is_action_pressed("LMB") and !dragging:
-		dragging = true
-		dragStartPos = $ScreenRef.get_local_mouse_position()
-	if event.is_action_released("LMB"):
-		dragging = false
+		
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -27,7 +20,3 @@ func _process(delta):
 	position.z +=( int(Input.is_action_pressed("out")) - int(Input.is_action_pressed("in")) ) * speed * delta
 	position.y +=( int(Input.is_action_pressed("up")) - int(Input.is_action_pressed("down")) ) * speed * delta
 	
-	if dragging :
-		dragMousePos = $ScreenRef.get_local_mouse_position()
-		rotation.y = (dragMousePos.x - dragStartPos.x) * 0.005
-		rotation.x = (dragMousePos.y - dragStartPos.y) * 0.005
